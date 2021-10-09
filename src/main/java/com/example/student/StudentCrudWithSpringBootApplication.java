4d02d1fa1f006d72cf1c2d5d31fcd82edaa668da
subhrapaladhi <subhrapaladhi9@gmail.com>
2021-10-09 21:41:25 +0530

    project init

---
+++ src/main/java/com/example/student/StudentCrudWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCrudWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/student/StudentCrudWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
