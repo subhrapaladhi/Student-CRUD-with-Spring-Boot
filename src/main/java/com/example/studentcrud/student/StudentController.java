6a245fbe80bfc9fabd25041845373eac058a44f5
subhrapaladhi <subhrapaladhi9@gmail.com>
2021-10-12 22:13:58 +0530

    update name or email

--- src/main/java/com/example/studentcrud/student/StudentController.java
+++ src/main/java/com/example/studentcrud/student/StudentController.java
@@ -29,4 +29,12 @@ public class StudentController {
   public void deleteStudent(@PathVariable("studentId") Long id) {
     studentService.deleteStudent(id);
   }
+
+  @PutMapping(path = "{studentId}")
+  public void updateStudent(
+    @PathVariable("studentId") Long id,
+    @RequestParam(required = false) String name,
+    @RequestParam(required = false) String email ) {
+    studentService.updateStudent(id, name, email);
+  }
 }
--- src/main/java/com/example/studentcrud/student/StudentService.java
+++ src/main/java/com/example/studentcrud/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.studentcrud.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,24 @@ public class StudentService {
       throw new IllegalStateException("student with id "+id+" does not exists");
     }
     studentRepository.deleteById(id);
+  }
+
+  @Transactional
+  public void updateStudent(Long id, String name, String email) {
+    Student student = studentRepository.findById(id)
+      .orElseThrow(() -> new IllegalStateException("student with id "+id+" not found"));
+
+    if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+    if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+      if(studentOptional.isPresent()) {
+        throw new IllegalStateException("Email taken");
+      }
+      student.setEmail(email);
+    }
+
+    studentRepository.save(student);
   }
 }
